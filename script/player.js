ce0988e9adc2b8073d7bea37342e1e0962504b6b
yuuki0405 <155883339+yuuki0405@users.noreply.github.com>
2025-09-02 12:17:28 +0900

    Update player.js

--- script/player.js
+++ script/player.js
@@ -1,1 +1,16 @@
+export const player = { x: 0, y: 0 };
 
+export function movePlayer(dir, map) {
+  let newX = player.x;
+  let newY = player.y;
+
+  if (dir === "up") newY--;
+  if (dir === "down") newY++;
+  if (dir === "left") newX--;
+  if (dir === "right") newX++;
+
+  if (map[newY] && map[newY][newX] === 0) {
+    player.x = newX;
+    player.y = newY;
+  }
+}
