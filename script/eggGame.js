88133dd18fb962d71dfd51237d04b87fc843c94b
s0u8a <mitoharu42024202@gmail.com>
2025-09-06 17:44:53 +0900

    Update eggGame.js

--- script/eggGame.js
+++ script/eggGame.js
@@ -36,7 +36,7 @@ export function startEggGame(onFinish) {
      style="
        width:100%;
        height:400px;
-       background:url('./assets/images/tanshigame.png') center/contain no-repeat;
+       background:url('./assets/images/tanshigame.png') center/cover no-repeat;
        position:relative;
        overflow:hidden;
        border:2px solid #900;">
